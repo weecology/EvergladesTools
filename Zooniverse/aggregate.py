60e83f677c1a1881fb7af026459b852835afbd4b
Ethan White <ethan@weecology.org>
2021-10-01 13:02:29 -0400

    Remove not yet functional dask client use

--- Zooniverse/aggregate.py
+++ Zooniverse/aggregate.py
@@ -398,7 +398,8 @@ def run(classifications_file=None, savedir=".", download=False, generate=False,m
 if __name__ == "__main__":
     #Download from Zooniverse and parse
     #Optional dask client
-    client = start_cluster.start(cpus=40, mem_size="8GB")
+    #client = start_cluster.start(cpus=40, mem_size="8GB")
+    client = None
     
     fname = run(savedir="../App/Zooniverse/data/", download=True, 
        generate=False, min_version=300, client=client, debug=False)
