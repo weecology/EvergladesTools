9dee9c6f480a054df4db35684694f90f98d05158
bw4sz <benweinstein2010@gmail.com>
2024-04-29 11:17:43 -0700

    add zip script

---
+++ Zooniverse/package_for_zenodo.py
@@ -0,0 +1,41 @@
+# Find the matching shp for each image for everglades data
+import os
+import glob
+import pandas as pd
+import zipfile
+
+base_folder = "/orange/ewhite/everglades/Zooniverse/parsed_images"
+
+# Find all shp files
+shp_files = glob.glob(os.path.join(base_folder, "shp", "*.shp"))
+
+# Find all .png files
+png_files = glob.glob(os.path.join(base_folder, "png", "*.png"))
+
+# Match the shp files to the png files
+shp_png = []
+for shp_file in shp_files:
+    shp_name = os.path.basename(shp_file)
+    png_name = shp_name.replace(".shp", ".png")
+    if os.path.join(base_folder, "png", png_name) in png_files:
+        shp_png.append((shp_name, png_name))
+
+# Print the number of unique files
+print("There are {} matched images".format(len(shp_png)))
+
+# Read in train and test csvs
+train = pd.read_csv(os.path.join(base_folder, "train.csv"))
+test = pd.read_csv(os.path.join(base_folder, "test.csv"))
+
+# Add the shapefiles to the zip
+zip_file = zipfile.ZipFile(os.path.join(base_folder, "shp_png.zip"), "w")
+for shp, png in shp_png:
+    zip_file.write(os.path.join(base_folder, "shp", shp), shp)
+    zip_file.write(os.path.join(base_folder, "png", png), png)
+
+# Add the train and test csvs to the zip
+zip_file.write("train.csv", "train.csv")
+zip_file.write("test.csv", "test.csv")
+
+zip_file.close()
+
